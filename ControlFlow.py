6d92e0efc8d178f0c0171e388153d8cf31f8a841
Jake Allen <jtallen6109@gmail.com>
2020-01-06 05:38:06 -0800

    Add Running Totals

--- ControlFlow.py
+++ ControlFlow.py
@@ -2,5 +2,10 @@
 Programmer: Jake Allen
 Date: 1-6-20
 Program: Running Total
-"""
 
+This program asks users for five numbers. It then sums up the given numbers
+"""
+sum = 0
+for i in range(5):
+    sum=int(input("Please enter a number: "))+sum
+print("\nThe sum of those numbers is: "+str(sum))

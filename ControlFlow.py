4e962697786738ed9a53803755b4ce327e9e7887
Jtallen2002 <58733957+Jtallen2002@users.noreply.github.com>
2020-01-08 05:05:51 -0800

    Average Test Updated
    Finished, unless told otherwise by teacher.

--- ControlFlow.py
+++ ControlFlow.py
@@ -10,3 +10,17 @@ howManyNumbers=int(input("How many numbers would you like to sum up: "))
 for i in range(howManyNumbers):
     sum=int(input("Please enter a number: "))+sum
 print("\nThe sum of those numbers is: "+str(sum))
+
+"""
+Programmer: Jake Allen
+Date: 1-8-20
+Program: Running Total, Part II
+
+This program asks users for how many tests they want to average, and then it takes their inputs for the test grades and then it spits out the average.
+"""
+
+average = 0
+howManyTests = int(input("How many tests would you like to average: "))
+for i in range(howManyTests):
+    average=int(input("Please enter the grade for test "+str(i+1)+": "))+average
+print("The average of those tests is "+str(float(average/howManyTests))+"%")
